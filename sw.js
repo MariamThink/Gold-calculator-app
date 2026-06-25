d7bc84e6dbf3b1eea73225bd6d0e079d1989d76d
MariamThink <mariamthink@gmail.com>
2026-06-25 19:09:09 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gold-calculator-v2026.06.25'; // always change this when you make updates
+const CACHE_NAME = 'gold-calculator-v2026.06.26'; // always change this when you make updates
 const urlsToCache = [
   '/Gold-calculator-app/',
   '/Gold-calculator-app/index.html',
